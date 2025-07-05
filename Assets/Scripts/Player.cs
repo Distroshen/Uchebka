348edc553bc992e200b4dc1db26c0c353ff89a11
Distroshen <danil.2005.maximov@gmail.com>
2025-07-05 13:17:48 +0400

    Движение, код
     в блендере быстренько созхдал арбузоподобюную модель. Сделал анимацию. Написал код, когда телеф то так, если пк то так, написал ему ускорение, создал гнома - противника. добавил, но в сценее его еще нет

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,112 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public float Speed = 3;
+    [SerializeField] private float MaxSpeed = 14;
+    [SerializeField] private float Speedz = 6;
+    [SerializeField] private float RightLimit = 1.75f;
+    [SerializeField] private float LeftLimit = -1.75f;
+
+    public bool isImmortal;
+    private bool isMobilePlatform;
+    private bool isTouching = false;
+    private float targetXPosition;
+
+    public bool Life = true;
+
     void Start()
     {
-        
+        Life = true;
+        // Определяем платформу
+        isMobilePlatform = Application.isMobilePlatform;
+
+        if (isMobilePlatform)
+        {
+            targetXPosition = transform.position.x;
+        }
+
+        isImmortal = false;
+        if (Life == true)
+        {
+            StartCoroutine(SS());
+        }
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Всегда двигаемся вперед
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.World);
+
+        if (isMobilePlatform)
+        {
+            MobileMovement();
+        }
+        else
+        {
+            PCMovement();
+        }
+    }
+
+    void PCMovement()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > LeftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * Speedz, Space.World);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < RightLimit)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * Speedz, Space.World);
+            }
+        }
+    }
+
+    void MobileMovement()
+    {
+        if (isTouching)
+        {
+            // Плавное движение к целевой позиции
+            float newX = Mathf.Lerp(transform.position.x, targetXPosition, Time.deltaTime * Speedz);
+            newX = Mathf.Clamp(newX, LeftLimit, RightLimit);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+    }
+
     void Update()
     {
-        
+        if (!isMobilePlatform) return;
+
+        // Обработка касаний для мобильных устройств
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                // Преобразуем позицию касания в мировые координаты
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                targetXPosition = touchPosition.x;
+                isTouching = true;
+            }
+        }
+        else
+        {
+            isTouching = false;
+        }
+    }
+    IEnumerator SS()
+    {
+        yield return new WaitForSeconds(7);
+        if (Speed < MaxSpeed)
+        {
+            Speed += 1;
+            StartCoroutine(SS());
+        }
     }
 }
